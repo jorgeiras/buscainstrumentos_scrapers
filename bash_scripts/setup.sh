a60f72baefed230f4df1f2bca7469bbda0d6e354
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-03-18 00:17:11 +0100

    test changing credentials

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -5,6 +5,10 @@ sudo apt install -y python3 python3-pip
 # scrapy setup
 pip3 install scrapy
 
+# reverb libraries setup 
+pip install requests_cache
+pip install ipython
+
 # selenium setup
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
@@ -19,10 +23,14 @@ git clone https://github.com/jorgeiras/buscainstrumentos_scrapers.git
 cd buscainstrumentos_scrapers
 cd scrapers
 
+# setup ES locale
+sudo locale-gen es_ES.UTF-8
+sudo update-locale
+
 # exec scraper scripts
-python3 guitarristasScrap.py
-python3 hispasonicScrap.py
-python3 reverbScrap.py
+#scrapy runspider guitarristasScrap.py -o guitarristas.csv -t csv
+#scrapy runspider hispasonicScrap.py -o hispasonic.csv -t csv
+#python3 reverbScrap.py
 python3 soundmarketScrap.py
 
 # get into the db loaders folder
@@ -30,7 +38,7 @@ cd ..
 cd db_loaders
 
 # exec db loader scripts
-python3 db_load_guitarristas.py
-python3 db_load_hispasonic.py
-python3 db_load_reverb.py
+#python3 db_load_guitarristas.py
+#python3 db_load_hispasonic.py
+#python3 db_load_reverb.py
 python3 db_load_soundmarket.py
